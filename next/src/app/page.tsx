307016887a6d17bf93d5287067dff2719de032a6
MariuzM <me@marius.dev>
2025-09-04 21:59:55 +0700

    add map

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Map } from '../components/Map/Map'
+
 export default function Home() {
-  return <div>Hello World</div>
+  return (
+    <>
+      <div>Hello World</div>
+      <Map />
+    </>
+  )
 }
--- react-tsr-start/src/routes/index.tsx
+++ react-tsr-start/src/routes/index.tsx
@@ -1,9 +1,16 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { Map } from '../components/Map/Map'
+
 export const Route = createFileRoute('/')({
   component: Home,
 })
 
 function Home() {
-  return <div>Hello World</div>
+  return (
+    <>
+      <div>Hello World</div>
+      <Map />
+    </>
+  )
 }
